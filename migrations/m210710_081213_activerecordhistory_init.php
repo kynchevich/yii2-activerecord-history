6262082c36729369900a8b1d662c5ee8d2c45394
Xiaomi <kirill.emko@mail.ru>
2021-07-10 12:53:55 +0300

    fix

--- migrations/m210710_081213_activerecordhistory_init.php
+++ migrations/m210710_081213_activerecordhistory_init.php
@@ -4,7 +4,7 @@ use yii\db\Migration;
 
 class m210710_081213_activerecordhistory_init extends Migration
 {
-    const TABLE_NAME = '{{%ar_model_history}}';
+    const TABLE_NAME = '{{%activerecord_models_history}}';
 
     public function up()
     {
@@ -14,7 +14,7 @@ class m210710_081213_activerecordhistory_init extends Migration
             'type' => $this->smallInteger()->notNull(),
             'model' => $this->string()->notNull(),
             'model_id' => $this->string()->notNull(),
-            'field_name' => $this->string()->notNull(),
+            'field_name' => $this->string(),
             'old_value' => $this->text(),
             'new_value' => $this->text(),
             'created_at' => $this->integer()->notNull(),
@@ -32,14 +32,14 @@ class m210710_081213_activerecordhistory_init extends Migration
 }
 
 /*
-CREATE TABLE IF NOT EXISTS `ar_model_history`
+CREATE TABLE IF NOT EXISTS `activerecord_models_history`
 (
     `id`         bigint       NOT NULL AUTO_INCREMENT,
     `user_id`    int(11),
     `type`       smallint     NOT NULL,
     `model`      varchar(255) NOT NULL,
     `model_id`   varchar(255) NOT NULL,
-    `field_name` varchar(255) NOT NULL,
+    `field_name` varchar(255),
     `old_value`  text,
     `new_value`  text,
     `created_at` int(11)      not null,
--- src/models/ActiveRecordHistory.php
+++ src/models/ActiveRecordHistory.php
@@ -3,6 +3,7 @@
 namespace kirillemko\activeRecordHistory\models;
 
 
+use App\domain\ACL\models\AclGroupsPermissions;
 use yii\behaviors\BlameableBehavior;
 use yii\behaviors\TimestampBehavior;
 
@@ -19,6 +20,11 @@ use yii\behaviors\TimestampBehavior;
  */
 class ActiveRecordHistory extends \yii\db\ActiveRecord
 {
+    public static function tableName()
+    {
+        return 'activerecord_models_history';
+    }
+
 
     const TYPE_INSERT = 1;
     const TYPE_UPDATE = 2;
@@ -38,5 +44,29 @@ class ActiveRecordHistory extends \yii\db\ActiveRecord
             ]
         ];
     }
+
+    public function fields()
+    {
+        return array_merge(parent::fields(),[
+            'field_full_name' => function() {
+                return $this->model::instance()->getAttributeLabel($this->field_name);
+            },
+            'type_full_name' => function() {
+                switch ($this->type){
+                    case ActiveRecordHistory::TYPE_INSERT:
+                        return 'Создание';
+                        break;
+                    case ActiveRecordHistory::TYPE_UPDATE:
+                        return 'Редактирование';
+                        break;
+                    case ActiveRecordHistory::TYPE_DELETE:
+                        return 'Удаление';
+                        break;
+                }
+                return '';
+            }
+        ]);
+    }
+
 
 }
