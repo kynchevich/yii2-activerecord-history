09784e40788a7049230280c1370f55718159383b
Kirill Emelianenko <kirill.emko@mail.ru>
2024-02-07 12:52:40 +0200

    manual history row adder added

---
+++ src/ActiveRecordHistoryAdder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace kirillemko\activeRecordHistory;
+
+
+use kirillemko\activeRecordHistory\models\ActiveRecordHistory;
+use yii\db\BaseActiveRecord;
+
+
+class ActiveRecordHistoryAdder
+{
+    protected ActiveRecordHistory $historyElement;
+
+    public function __construct()
+    {
+        $this->historyElement = new ActiveRecordHistory();
+    }
+
+    public function setModel(string $className): self
+    {
+        $this->historyElement->model = $className;
+        return $this;
+    }
+
+    public function setModelId($modelId): self
+    {
+        $this->historyElement->model_id = $modelId;
+        return $this;
+    }
+
+    public function setEvent(string $eventName): self
+    {
+        $this->historyElement->event = $eventName;
+        return $this;
+    }
+
+    public function setOldValue($oldValue): self
+    {
+        $this->historyElement->old_value = $oldValue;
+        return $this;
+    }
+
+    public function setNewValue($newValue): self
+    {
+        $this->historyElement->old_value = $newValue;
+        return $this;
+    }
+
+    public function save(): bool
+    {
+        return $this->historyElement->save();
+    }
+
+}
